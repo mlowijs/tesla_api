5522e02525a2dc9d913938c2ef3dec3be862026d
Michiel Lowijs <michiel.lowijs@sycade.com>
2018-10-01 15:40:14 +0200

    whoops

--- tesla_api/__init__.py
+++ tesla_api/__init__.py
@@ -84,12 +84,7 @@ class TeslaApiClient:
         if 'error' in response_json:
             raise ApiError(response_json['error'])
 
-        api_response = response_json['response']
-
-        if not api_response['result']:
-            raise ApiError(api_response['reason'])
-
-        return api_response
+        return response_json['response']
 
     def list_vehicles(self):
         return [Vehicle(self, vehicle) for vehicle in self.get('vehicles')]

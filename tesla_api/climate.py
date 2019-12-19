fba6202e3495f243e6a4104dbe3fa6aaf348bcc1
erikfa <erik@fagersten.no>
2019-12-19 14:09:42 +0100

    Support for seat and steering wheel heating
    Added support for heating seats and steering wheel

--- tesla_api/climate.py
+++ tesla_api/climate.py
@@ -18,3 +18,20 @@ class Climate:
             {'driver_temp': driver_temperature,
              'passenger_temp': passenger_temperature or driver_temperature}
         )
+    
+    def set_seat_heater(self,temp=0,seat=0):
+        # temp = The desired level for the heater. (0-3)
+        # The desired seat to heat. (0-5)
+        # 0 - Driver
+        # 1 - Passenger
+        # 2 - Rear left
+        # 4 - Rear center
+        # 5 - Rear right
+        return self._api_client.post('vehicles/{}/command/remote_seat_heater_request'.format(self._vehicle_id),{'heater':seat,'level':temp})
+
+    def start_steering_wheel_heater(self):
+        return self._api_client.post('vehicles/{}/command/remote_steering_wheel_heater_request'.format(self._vehicle_id),{'on':True})
+
+    def stop_steering_wheel_heater(self):
+        return self._api_client.post('vehicles/{}/command/remote_steering_wheel_heater_request'.format(self._vehicle_id),{'on':False})
+    

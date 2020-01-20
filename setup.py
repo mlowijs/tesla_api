2311da05dce66ac0c3628dfe7c66c9401cc12cfa
Michiel Lowijs <mlowijs@gmail.com>
2020-01-20 09:48:43 +0100

    Version bump to 2.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tesla_api",
-    version="1.1.0",
+    version="2.0.0",
     author="M. Lowijs",
     author_email="mlowijs@gmail.com",
     description="API client for Tesla",

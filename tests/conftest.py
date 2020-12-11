a8a879976f1f441d1eeb1038d24de623bf2ed50f
Hellowlol <Hellowlol@users.noreply.github.com>
2020-12-11 01:50:01 +0100

    patch _command

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 from data import (CHARGE_STATE, DRIVE_STATE, FULL_DATA, GUI_SETTINGS,
                   VEHICLE_STATE)
@@ -5,7 +7,7 @@ from tesla_api import TeslaApiClient
 from tesla_api.vehicle import Vehicle
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def client():
 
     fake_client = TeslaApiClient("fake@email.com", "fake_password")
@@ -15,4 +17,12 @@ def client():
 
 @pytest.fixture()
 def vehicle(client, mocker):
-    return Vehicle(client, FULL_DATA)
+
+    v = Vehicle(client, FULL_DATA)
+    # never for the the mock to work..
+    # just patching this manually since it will not work anyway.
+    async def _command(command_endpoint, data=None, _retry=True):
+        return True
+    v._command = _command
+
+    return v
--- tests/test_doors.py
+++ tests/test_doors.py
@@ -8,3 +8,15 @@ async def test_Vehicle_Status_Doors(vehicle):
     assert doors.front_passanger_door_open is False
     assert doors.rear_driver_door_open is False
     assert doors.rear_passenger_door_open is False
+
+
+@pytest.mark.asyncio
+async def test_Vehicle_Status_Doors_lock(vehicle):
+    doors = vehicle.controls.doors
+    assert await doors.lock() is True
+
+
+@pytest.mark.asyncio
+async def test_Vehicle_Status_Doors_unlock(vehicle):
+    doors = vehicle.controls.doors
+    assert await doors.unlock() is True

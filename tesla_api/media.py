e8163c94b409ed34a61ee4d80026f20888383ea0
Hellowlol <Hellowlol@users.noreply.github.com>
2020-12-14 23:58:35 +0100

    Add typing to media

--- tesla_api/media.py
+++ tesla_api/media.py
@@ -4,7 +4,7 @@ from .base import Stub
 class Media(Stub):
 
     @property
-    def center_display(self):
+    def center_display(self) -> str:
         cd = {
             0: "off",
             2: "normal on",
@@ -15,26 +15,26 @@ class Media(Stub):
 
         return cd[self._vehicle._data["vehicle_state"]["center_display_state"]]
 
-    async def toggle_media_playback(self):
+    async def toggle_media_playback(self) -> bool:
         return await self._vehicle._command('media_toggle_playback')
 
-    async def next_track(self):
+    async def next_track(self) -> bool:
         return await self._vehicle._command('media_next_track')
 
-    async def prev_track(self):
+    async def prev_track(self) -> bool:
         return await self._vehicle._command('media_prev_track')
 
-    async def next_fav(self):
+    async def next_fav(self) -> bool:
         return await self._vehicle._command('media_next_fav')
 
-    async def prev_fav(self):
+    async def prev_fav(self) -> bool:
         return await self._vehicle._command('media_prev_fav')
 
-    async def volume_up(self):
+    async def volume_up(self) -> bool:
         return await self._vehicle._command('media_volume_up')
 
-    async def volume_down(self):
+    async def volume_down(self) -> bool:
         return await self._vehicle._command('media_volume_down')
 
-    def __getattr__(self, name):
+    def __getattr__(self, name: str) -> bool:
         return self._vehicle._data["vehicle_state"]["media_state"][name]

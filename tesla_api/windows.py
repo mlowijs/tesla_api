b07dd0f82cf8d41210097f130edc87ae97df717e
Hellowlol <Hellowlol@users.noreply.github.com>
2020-12-15 20:55:16 +0100

    Windows typing and doc strings

--- tesla_api/windows.py
+++ tesla_api/windows.py
@@ -1,37 +1,84 @@
 
+
+
 from .base import Stub
 from .misc import cast
 
 
 class Windows(Stub):
 
-    async def vent(self, lat=0, lon=0):
-        """Controls the windows. Will vent all windows simultaneously."""
+    async def vent(self, lat: float = 0, lon: float = 0) -> bool:
+        """Ventilate the Windows
+
+
+        Note:
+            The lat and lon values are ignored by the api.
+
+        Args:
+            lat (float, optional): latitude
+            lon (float, optional): longitude
+
+        Returns:
+            bool: Returns True if success
+        """
+        lat = lat or self._vehicle.status.latitude
+        lon = lon or self._vehicle.status.longitude
         return await self._vehicle._command(
             "window_control", data={"command": "vent", "lat": lat, "lon": lon}
         )
 
-    async def close(self, lat: float = 0.0, lon: float = 0.0):
-        """Controls the windows. Will close all windows simultaneously."""
-        ds = self._vehicle._data["drive_state"]
-        lat = lat or ds.get("latitude")
-        lon = lon or ds.get("longitude")
+
+    async def close(self, lat: float = 0, lon: float = 0) -> bool:
+        """Close the Windows.
+
+        Note:
+            Must be near the current location of the car
+
+        Args:
+            lat (float, optional): lat must be near the current location of the car.
+            lon (float, optional): lon must be near the current location of the car.
+
+        Returns:
+            bool: Returns True if success
+        """
+        lat = lat or self._vehicle.status.latitude
+        lon = lon or self._vehicle.status.longitude
         return await self._vehicle._command(
             "window_control", data={"command": "close", "lat": lat, "lon": lon}
         )
 
     @property
     def driver_window_open(self) -> bool:
+        """Driver window open
+
+        Returns:
+            bool: True if the window is open, False it not
+        """
         return cast(self._vehicle._data["vehicle_state"]["fd_window"])
 
     @property
     def passenger_window_open(self) -> bool:
+        """Passenger window open
+
+        Returns:
+            bool: True if the window is open, False it not
+        """
         return cast(self._vehicle._data["vehicle_state"]["fp_window"])
 
     @property
     def rear_driver_window_open(self) -> bool:
+        """Rear driver window open
+
+        Returns:
+            bool: True if the window is open, False it not
+        """
         return cast(self._vehicle._data["vehicle_state"]["rd_window"])
 
     @property
     def rear_passenger_window_open(self) -> bool:
+        """Rear passenger window open
+
+        Returns:
+            bool: True if the window is open, False it not
+        """
         return cast(self._vehicle._data["vehicle_state"]["rp_window"])

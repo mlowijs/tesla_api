209c9137c7662307317ccf7175c55d58b14b0b78
Hellowlol <Hellowlol@users.noreply.github.com>
2020-12-16 10:04:05 +0100

    Docstring software.py

--- tesla_api/software.py
+++ tesla_api/software.py
@@ -1,16 +1,26 @@
 from datetime import datetime, timedelta
+from typing import Union
 
 from .base import Stub
 
 
-from typing import Union
 class Software(Stub):
     @property
     def update_available(self) -> bool:
+        """Check if a update is available
+
+        Returns:
+            bool: True if a update is available
+        """
         return self.download_percent == 100
 
     @property
     def done_at(self) -> datetime:
+        """What time the update is expected to be done.
+
+        Returns:
+            datetime: What time the update is expected to be done.
+        """
         sec = self._vehicle._data["vehicle_state"]["software_update"][
             "expected_duration_sec"
         ]
@@ -18,25 +28,32 @@ class Software(Stub):
 
     @property
     def install_percent(self) -> int:
+        """How far along the install is"""
         return self._vehicle._data["vehicle_state"]["software_update"]["install_perc"]
 
     @property
     def download_percent(self) -> int:
+        """How far along the install is"""
         return self._vehicle._data["vehicle_state"]["software_update"]["download_perc"]
 
     @property
     def status(self) -> str:
+        """Status""" # TODO check what is listed here before the next update.
         return self._vehicle._data["vehicle_state"]["software_update"]["status"]
 
     @property
     def version(self) -> str:
+        """What version is the update."""
         return self._vehicle._data["vehicle_state"]["software_update"]["version"]
 
     async def update(self, offset: Union[datetime, int] = 120) -> bool:
-        """Start a update as a time in the future.
+        """Start a update.
+
+        Note:
+            Carefull running this as you can't drive the car until it's done.
 
         Args:
-            offset (int, datetime)
+            offset (int, datetime): When the update should start.
 
         """
         if isinstance(offset, datetime):

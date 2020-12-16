f90e7af7a9ed8a176786030771b9bfac134de1c3
Hellowlol <Hellowlol@users.noreply.github.com>
2020-12-16 09:57:52 +0100

    Docstring sentry

--- tesla_api/sentry.py
+++ tesla_api/sentry.py
@@ -6,16 +6,26 @@ from .base import Stub
 class Sentry(Stub):
     @property
     def is_active(self) -> bool:
+        """"Returns True if sentry mode is active"""
         return self._vehicle._data["vehicle_state"]["sentry_mode"]
 
     @property
     def is_available(self) -> bool:
+        """Returns True if sentry mode is available """
         return self._vehicle._data["vehicle_state"]["sentry_mode_available"]
 
-    # Should probable check and rename methods
-    # I want them to be as close to the class as possible.
     async def enable(self, on: bool = True) -> bool:
-        """Enable sentry mode."""
+        """Enable sentry mode
+
+        Args:
+            on (bool, optional): True to enable and False to disable.
+
+        Returns:
+            bool: True of the command succeeds
+        """
         return await self._vehicle._command("set_sentry_mode", data={"on": on})
 
-    disable = partialmethod(enable, on=False)
+
+    async def disable(self, on: bool = True) -> bool:
+        """Disable sentry mode."""
+        return await self.enable(on=False)

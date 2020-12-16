d9905eda1eeef07e7e0d07c7d2872d7dd414c174
Sam Bull <aa6bs0@sambull.org>
2020-12-16 14:28:18 +0000

    Update controls.py

--- tesla_api/controls.py
+++ tesla_api/controls.py
@@ -1,7 +1,6 @@
-import asyncio
 from enum import Enum
 from functools import partialmethod
-from typing import cast, Literal, TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
 
 if TYPE_CHECKING:
     from .vehicle import Vehicle

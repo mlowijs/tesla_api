510dad6a2f646edd9a814487b4f69c7eb9426ced
Hellowlol <Hellowlol@users.noreply.github.com>
2020-12-16 09:50:05 +0100

    Doc string and remove
    and remove s for some properties as it not plural

--- tesla_api/gui.py
+++ tesla_api/gui.py
@@ -4,26 +4,53 @@ from .base import Stub
 
 
 class Gui(Stub):
+
+    """Gui related thing like what format the Vehicle uses for range, time etc
+    """
+
     __key = "gui_settings"
 
     @property
     def twenty_four_hour(self) -> bool:
+        """Returns True if the car uses 00:00 format."""
         return self._vehicle._data[self.__key]["gui_24_hour_time"]
 
     @property
-    def charge_rate_units(self) -> str:
+    def charge_rate_unit(self) -> str:
+        """What charge rate unit is used on the gui
+
+        Returns:
+            str: kW, km/hr m/hr
+        """
         return self._vehicle._data[self.__key]["gui_charge_rate_units"]
 
     @property
     def distance_unit(self) -> str:
+        """Distance unit the vehicle uses in the gui
+
+        Note: Not sure about the the american format, mph, mi/h
+
+        Returns:
+            str: km/hr, m/hr
+        """
         return self._vehicle._data[self.__key]["gui_distance_units"]
 
     @property
     def temperature_unit(self) -> str:
+        """temperature_unit the vehicle uses in the gui
+
+        Returns:
+            str: C or F
+        """
         return self._vehicle._data[self.__key]["gui_temperature_units"]
 
     @property
-    def range_units(self) -> bool:
+    def range_unit(self) -> bool:
+        """Range unit
+
+        Returns:
+            bool: Rated, todo list the other values...
+        """
         return self._vehicle._data[self.__key]["gui_range_display"]
 
     @property
--- tesla_api/media.py
+++ tesla_api/media.py
@@ -3,8 +3,16 @@ from .base import Stub
 
 class Media(Stub):
 
+    """Stuff related to the Media and media playback.
+    """
+
     @property
     def center_display(self) -> str:
+        """Indicate what's on the screen.
+
+        Returns:
+            str: Possible values off, normal on, charing screen, sentry mode, dog mode
+        """
         cd = {
             0: "off",
             2: "normal on",
@@ -16,24 +24,59 @@ class Media(Stub):
         return cd[self._vehicle._data["vehicle_state"]["center_display_state"]]
 
     async def toggle_media_playback(self) -> bool:
+        """Toggle media playback
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_toggle_playback')
 
     async def next_track(self) -> bool:
+        """Skip to next track
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_next_track')
 
     async def prev_track(self) -> bool:
+        """Jump back the previous track
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_prev_track')
 
     async def next_fav(self) -> bool:
+        """Jump to next favorite
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_next_fav')
 
     async def prev_fav(self) -> bool:
+        """Jump to previous favorite
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_prev_fav')
 
     async def volume_up(self) -> bool:
+        """Increase the volume
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_volume_up')
 
     async def volume_down(self) -> bool:
+        """Decrease the volume
+
+        Returns:
+            bool: True if successfull
+        """
         return await self._vehicle._command('media_volume_down')
 
     def __getattr__(self, name: str) -> bool:
